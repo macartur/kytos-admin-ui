df24a6484ad0f9dcae8d69072c27a6d6df8e186b
José Luiz Coe <joseluizcoe@gmail.com>
2016-11-01 12:58:11 -0300

    Adicionando comportamento da barra lateral.

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -1,5 +1,11 @@
 ;(function() {
 
-$(".terminal").kterminal();
+  $(".terminal").kterminal();
+
+  $(".left-nav-toggle>a").click(function(e){
+    e.preventDefault();
+    $("body").toggleClass("mobile");
+    return false;
+  })
 
 }());
